e45f54e58b7df01901e383a73a8ae34872fbc29c
DavidAlejandro123 <alejandroariste210@gmail.com>
2024-07-09 14:07:31 -0400

    second commit refactorizacionPythonTarea

--- tarea.py
+++ tarea.py
@@ -8,23 +8,25 @@ def menu():
 #metodo para introducir calificaciones y comentarios
 def introducirCalificaciones():
     while True:
-                print('Por favor, introduzca una puntuación en una escala de 1 a 5')
+        print('Por favor, introduzca una puntuación en una escala de 1 a 5')
+        point = input()
+        if point.isdecimal():
+            point = int(point)
+            while (point <= 0 or point > 5): # se agrega un bucle while para que verifique el valor
+                print('Por favor, introduzca un valor entre el 1 y 5')
                 point = input()
-                if point.isdecimal():
-                    point = int(point)
-                    if  point <= 0 or point > 5: # Expresión condicional de menor que 0 o mayor que 5.
-                        print('Por favor, introduzca un valor entre el 1 y 5')
-                        point = input()
-                    else:
-                        print('Introduzca sus comentarios.')
-                        comment = input()
-                        post = f'punto: {point} comentario: {comment}'
-                        file_pc = open("data.txt", 'a')
-                        file_pc.write(f'{ post } \n')
-                        file_pc.close()
-                        break
-                else:
-                    print('Introduzca los puntos de valoración como números')
+                point = int(point)      #se convierte a entero el valor ingresado
+                
+            else:
+                print('Introduzca sus comentarios.')
+                comment = input()
+                post = f'punto: {point} comentario: {comment}'
+                file_pc = open("data.txt", 'a')
+                file_pc.write(f'{ post } \n')
+                file_pc.close()
+                break
+        else:
+            print('Introduzca los puntos de valoración como números')
 
 #metodo para mostrar calificaciones y comentarios
 def mostrarCalificaciones():
